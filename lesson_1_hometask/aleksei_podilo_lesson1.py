900d1d67ea1ad1b07f6a237641bf8f3a689ee0ff
bat41k <bat41k@protonmail.com>
2022-01-30 00:02:37 +0300

    Создавали переменные и производили операции над ними

---
+++ lesson_1_hometask/aleksei_podilo_lesson1.py
@@ -0,0 +1,25 @@
+###Задание #1
+a = 1
+b = 2
+string_rus = "Привет, мир!"
+string_eng = "Hello, world!"
+my_list = [1, 2, 3, 4, 5]
+
+###Задание #2
+с = a + b
+c_1 = a ** b
+c_2 = a % b
+c_3 = a * b
+c_4 = a / b
+c_5 = a // b
+
+###Задание #3
+my_list_1 = my_list[1:4]
+
+###Задание #4
+string_rus_eng = (string_rus + " " + string_eng)
+
+###Задание #5
+just_dict = {"name": "Aleksei", "age": 20}
+just_dict["name"] = "Tanja"
+age = just_dict["age"]
