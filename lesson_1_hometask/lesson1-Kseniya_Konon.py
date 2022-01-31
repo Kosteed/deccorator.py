28cda8c08dc65b830a673608c8e15a2711b1653b
Kseniya2801 <kseniya.konon@mail.ru>
2022-01-31 22:18:32 +0300

    gkgkgkgkgkg

---
+++ lesson_1_hometask/lesson1-Kseniya_Konon.py
@@ -0,0 +1,46 @@
+#Задание1
+a = 7
+b = 23
+wave = [5, 10, 15, 20, 25, 30]
+name = 'Kseniya'
+last_name = 'Konon'
+print('a =', a)
+print('b = ', b)
+print('wave =', wave)
+print('name =', name)
+print('last_name  =', last_name)
+
+
+# Задание2
+result1 = a + b
+print(result1)
+result2 = a * b 
+print(result2)
+result3 = b ** a
+print(result3)
+result4 = b / a 
+print(result4)
+result5 = b // a
+print(result5)
+result6 = b % a 
+print(result6) 
+
+
+
+# Задание 3
+wave1 = wave[1:4]
+print('wave1 = ', wave1)
+
+# Задание4
+full_name = name + ' ' + last_name
+print('full_name = ', full_name)
+
+
+# Задание5
+just_dict = {'name': 'Kseniya', 'age': 25}
+print(just_dict)
+just_dict['name'] = 'cat'
+print(just_dict)
+age = just_dict['age']
+print(age)
+print('Name =', just_dict['name'], ' , Age =', just_dict['age'])
